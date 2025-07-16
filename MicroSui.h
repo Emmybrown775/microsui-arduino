2e1d92ccbcdc220a548870b3f7c74e13acb0f6dc
Emmybrown775 <elumezeemma@gmail.com>
2025-07-16 14:05:42 +0100

    First Commit

---
+++ MicroSui.h
@@ -0,0 +1,20 @@
+#ifndef MICROSUI_H
+#define MICROSUI_H
+
+#include "sign.h"
+#include "utils.h"
+#include "cryptography.h"
+
+#include "compact25519/compact_ed25519.c"
+#include "compact25519/compact_x25519.c"
+#include "compact25519/compact_wipe.c"
+#include "compact25519/c25519/c25519.c"
+#include "compact25519/c25519/ed25519.c"
+#include "compact25519/c25519/edsign.c"
+#include "compact25519/c25519/f25519.c"
+#include "compact25519/c25519/fprime.c"
+#include "compact25519/c25519/sha512.c"
+#include "monocypher/monocypher.c"
+
+
+#endif
